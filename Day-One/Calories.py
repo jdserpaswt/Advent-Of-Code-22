a7fe5a9f20815e2abb9eb2abc35167eadaf5b15a
jdserpaswt <josh.serpas@willowtreeapps.com>
2022-12-01 10:58:42 -0500

    Day 1 Part 2

--- Day-One/Calories.py
+++ Day-One/Calories.py
@@ -1,16 +1,26 @@
+from heapq import heappop, heappush
+
+
 with open('data/input.txt', 'r') as f:
     data = [l.strip() for l in f.readlines()]
-    greatest = 0
+    calories = []
     current = 0
     for s in data:
         #if we hit the end of an elf's list we reset counter
+        #negate and add to heap (min-heap)
         if not s:
+            heappush(calories, current * -1)
             current = 0
             continue
         #we assume that all input is clean and always a valid int
         current += int(s)
-        #we check to see if the current number is the largest we've counted so far
-        if current > greatest:
-            greatest = current
     
-    print("The largest sum is: ", greatest)
+    elf_one = heappop(calories) * -1
+    elf_two = heappop(calories) * -1
+    elf_three = heappop(calories) * -1
+
+    print("The largest sum is: ", elf_one)
+    print("The second sum is: ", elf_two)
+    print("The third sum is: ", elf_three)
+
+    print("The three elves are carrying a total of: ", elf_one + elf_two + elf_three)
